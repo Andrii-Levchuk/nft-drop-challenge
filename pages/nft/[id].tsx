1b76ed1f7d04848de8c0950f5c21653873091d26
Andrii Levchuk <andriykolevchuk@gmail.com>
2022-10-08 20:38:27 +0200

    finally completed

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import { useAddress, useDisconnect, useMetamask } from '@thirdweb-dev/react'
+import { GetServerSideProps } from 'next'
+import { sanityClient, urlFor } from '../../sanity'
+import { Collection } from '../../typing'
+import { url } from 'inspector'
+import Link from 'next/link'
 
-function NFTDropPage() {
+interface Props {
+	collection: Collection
+}
+
+
+function NFTDropPage({collection}: Props) {
   
 
 // auth
@@ -19,15 +29,15 @@ console.log(address)
 					<div className='rounded-xl bg-gradient-to-br from-yellow-400 to-purple-600 p-2'>
 						<img
 							className='w-44 rounded-xl object-cover lg:h-96 lg:w-72'
-							src='https://links.papareact.com/8sg'
+							src={urlFor(collection.previewImage).url()}
 							alt='image'
 						/>
 					</div>
 					<div className='text-center p-5 space-y-2'>
-						<h1 className='text-4xl text-white font-bold'>ALEVCHUK Apes</h1>
-						<h2 className='text-xl text-gray-300'>
-							A collection of ALEVCHUK Apes who live & breathe React!
-						</h2>
+						<h1 className='text-4xl text-white font-bold'>
+							{collection.NFTCollection}
+						</h1>
+						<h2 className='text-xl text-gray-300'>{collection.description}</h2>
 					</div>
 				</div>
 			</div>
@@ -36,13 +46,15 @@ console.log(address)
 			<div className='flex flex-1 flex-col p-12 lg:col-span-6'>
 				{/* Header */}
 				<header className='flex items-center justify-between'>
-					<h1 className='w-52 cursor-pointer text-xl font-extralight sm:w-80'>
-						The{' '}
-						<span className='font-extrabold underline decoration-pink-600'>
-							ALEVCHUK
-						</span>{' '}
-						NFT Marker Place
-					</h1>
+					<Link href={'/'}>
+						<h1 className='w-52 cursor-pointer text-xl font-extralight sm:w-80'>
+							The{' '}
+							<span className='font-extrabold underline decoration-pink-600'>
+								ALEVCHUK
+							</span>{' '}
+							NFT Marker Place
+						</h1>
+					</Link>
 					<button
 						onClick={() => (address ? disconnect() : connectWithMetamask())}
 						className='rounded-full bg-gradient-to-br from-cyan-800 to-rose-500 px-4 py-2 text-xs text-white font-bold lg:px-5 lg:py-3 lg:text-base'
@@ -62,12 +74,12 @@ console.log(address)
 				<div className='mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:space-y-0 lg:justify-center'>
 					<img
 						className='w-80 object-cover pb-10 lg:h-40'
-						src='https://links.papareact.com/bdy'
+						src={urlFor(collection.mainImage).url()}
 						alt='photo'
 					/>
 
 					<h1 className='text-3xl font-bold lg:tetx-5xl lg:font-extrabold'>
-						The ALEVCHUK Ape Code Mastering | NFT Drop
+						{collection.title}
 					</h1>
 					<p className='pt-2 text-xl text-green-500 '>13 / 21 NFT's claimed</p>
 				</div>
@@ -81,3 +93,42 @@ console.log(address)
 }
 
 export default NFTDropPage
+
+export const getServerSideProps: GetServerSideProps = async({params}) => {
+const query = `*[_type == "collection" && slug.current == $id][0] {
+  _id,
+  title,
+  address,
+  description,
+  NFTCollection,
+  mainImage {
+    asset
+  },
+  previewImage {
+    asset
+  },
+  slug {
+    current
+  },
+  creator-> {
+    _id,
+    name,
+    address,
+    slug {
+      current
+    },
+  },
+}`
+const collection = await sanityClient.fetch(query, {id:params?.id})
+
+if (!collection) {
+	return {
+		notFound:true
+	}
+}
+return {
+	props: {
+		collection,
+	},
+}
+}
